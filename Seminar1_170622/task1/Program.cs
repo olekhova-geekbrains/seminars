755d037d4abe3672858e69f266fe81d85398f92a
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-17 14:41:49 +0300

    add task1

---
+++ Seminar1_170622/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ввести два числа. Проверить является ли первое число квадратом второго
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2 * num2) {
+  Console.WriteLine("да");
+}
+else {
+  Console.WriteLine("нет");
+}
