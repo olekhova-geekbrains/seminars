55f35d3819353e01cc1eb8ea19b5c300a108af95
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-19 23:17:13 +0300

    add task8

---
+++ Homework1/task8/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 2)
+{
+  Console.WriteLine("Таких чисел нет");
+}
+else
+{
+  int i = 2;
+  while (i <= num) {
+    Console.Write($"{i}, ");
+    i += 2;
+  }
+}
