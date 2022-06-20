fe475ebee55ea867d159a7bb16aec18430d91173
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-20 11:03:00 +0300

    redone task4

--- Homework1/task4/Program.cs
+++ Homework1/task4/Program.cs
@@ -11,26 +11,14 @@ Console.WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int num3 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2)
+
+int max = num1;
+if (num2 > max)
 {
-  if (num1 > num3) 
-  {
-    Console.WriteLine($"max = {num1}");
-  }
-  else if (num2 > num3)
-  {
-    Console.WriteLine($"max = {num2}");
-  }
-  else 
-  {
-    Console.WriteLine($"max = {num3}");
-  }
+  max = num2;
 }
-else if (num2 > num3)
+if (num3 > max)
 {
-  Console.WriteLine($"max = {num2}");
+  max = num3;
 }
-else
-{
-  Console.WriteLine($"max = {num3}");
-}
+Console.WriteLine($"max = {max}");
