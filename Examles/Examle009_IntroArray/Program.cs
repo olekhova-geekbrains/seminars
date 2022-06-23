ca8555f70aa72adf0cbca6ef164a2c18fb234b48
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-23 13:44:39 +0300

    add examle009

--- Examles/Examle009_IntroArray/Program.cs
+++ Examles/Examle009_IntroArray/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти максимум из 9 элементов
+
+int Max(int arg1, int arg2, int arg3)
+{
+  int result = arg1;
+  if (arg2 > result) result = arg2;
+  if (arg3 > result) result = arg3;
+  return result;
+}
+
+int[] array = {1, 32, 93, 64, 5, 67, 75, 58, 39};
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+  Max(array[0], array[1], array[2]),
+  Max(array[3], array[4], array[5]),
+  Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
