88e7d63049231f87e2abf0d61b896257923874a3
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-23 13:55:59 +0300

    add examle010

---
+++ Examles/Examle010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 34, 5, 5, 6, 845, 245 };
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
