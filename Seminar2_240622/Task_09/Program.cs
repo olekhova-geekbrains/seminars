50102b3128fa2cd393f18330738849cc146151f0
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-24 11:04:37 +0300

    add task_09

---
+++ Seminar2_240622/Task_09/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное число из диапозона [10, 99]
+// и выводит наибольшуюю цифру этого числа
+
+int rndNum = new Random().Next(10,100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
+
+// Console.Write($"Наибольшая цифра числа {rndNum} => ");
+
+// int result = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine(result);
+
+int MaxDigit(int num)
+{
+  int firstDigit = num / 10;
+  int secondDigit = num % 10;
+  if (firstDigit == secondDigit) return 0;
+  return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+// rndNum = 88;
+
+int maxDigit = MaxDigit(rndNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "цифры одинаковые";
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
