34d46b8b600d5c1079ef3b70a38125feb70aa0a5
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-24 17:51:22 +0300

    add task_11

---
+++ Seminar2_240622/Task_11/Program.cs
@@ -0,0 +1,10 @@
+﻿// 11.Напишите программу,
+//  которая выводит случайное трёхзначное число и 
+//  удаляет вторую цифру этого числа. 456 -> 46 782 -> 72 918 -> 98 
+
+int rndNum = new Random().Next(10, 100);
+int rmSecDigit(int num)
+{
+  int firstGigit = num / 100;
+  int thirdDigit = num % 10;
+}
