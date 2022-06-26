e44e2994523bba3b957b3021158ac47c7603b3ff
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-26 22:45:34 +0300

    add Task_10

---
+++ Homework2/Task_10/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int num)
+{
+  bool isNotThreeDigit = num / 100 == 0 || Math.Abs(num / 100) >= 10;
+  if (isNotThreeDigit) return 0;
+  int secondDigit = Math.Abs((num % 100) / 10);
+  return secondDigit;
+}
+
+int secondDigit = SecondDigit(number);
+string result = secondDigit > 0 ? $"Вторая цифра числа {number}: {secondDigit}" : "Это не трёхзначное число";
+Console.WriteLine(result);
