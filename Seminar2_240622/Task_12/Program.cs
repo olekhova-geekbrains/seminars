6819a015e5f35ca7d4302f95a5b31212f7e5a864
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-26 22:12:20 +0300

    add Task_12

--- Seminar2_240622/Task_12/Program.cs
+++ Seminar2_240622/Task_12/Program.cs
@@ -1,3 +1,17 @@
 ﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
 // является ли второе число кратным первому. Если число 2 не кратно числу 1, 
 // то программа выводит остаток от деления. 34, 5 -> не кратно, остаток 4 16, 4 -> кратно 
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+void IsMultiple(int a, int b)
+{
+  int rem = a % b;
+  if (rem == 0) Console.WriteLine("кратно");
+  else Console.WriteLine($"не кратно, остаток {rem}");
+}
+
+IsMultiple(num1, num2);
