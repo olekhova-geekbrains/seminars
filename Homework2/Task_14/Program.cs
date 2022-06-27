0bb73c1ed894ab9777f21a36b2d3d9f583dd6261
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-27 11:33:41 +0300

    add Task_14

---
+++ Homework2/Task_14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 14: Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string IsMultiple(int num)
+{
+  if (num % 7 == 0 && num % 23 == 0) return "да";
+  return "нет";
+}
+
+Console.WriteLine(IsMultiple(number));
