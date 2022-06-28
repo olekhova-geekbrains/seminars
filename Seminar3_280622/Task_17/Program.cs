b3fd4ee26862d049b09a9358782ad7c64827e7e9
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-28 10:35:27 +0300

    add task 17

---
+++ Seminar3_280622/Task_17/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Введите координаты точки X и Y");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+// if (x > 0 && y > 0)
+// {
+//   Console.WriteLine("1 четверть");
+
+// }
+// else if (x < 0 && y > 0)
+// {
+//   Console.WriteLine("2 четверть");
+// }
+// else if (x < 0 && y < 0)
+// {
+//   Console.WriteLine("3 четверть");
+// }
+// else if (x > 0 && y < 0)
+// {
+//   Console.WriteLine("4 четверть");
+// }
+// else
+// {
+//   Console.WriteLine("Введены некорректные координаты");
+// }
+
+string Quard(int xc, int yc)
+{
+  if (xc > 0 && yc > 0) return "1 четверть";
+  if (xc < 0 && yc > 0) return "2 четверть";
+  if (xc < 0 && yc < 0) return "3 четверть";
+  if (xc > 0 && yc < 0) return "4 четверть";
+  else  return "Введены некорректные координаты";
+}
+string result = Quard(x, y);
+Console.WriteLine(result);
