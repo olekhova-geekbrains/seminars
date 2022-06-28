f09f3cd0589a36dd5f69fe9d83d9f9a3a7d5b9db
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-28 11:01:59 +0300

    add Task_18

---
+++ Seminar3_280622/Task_18/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 18: Напишите программу, которая по 
+// заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой 
+// четверти (x и y). 
+
+Console.WriteLine("Введите номер четверти");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string Coord(int a)
+{
+  string res = "";
+  if (a == 1) res = "x > 0 и y > 0";
+  if (a == 2) res = "x < 0 и y > 0";
+  if (a == 3) res = "x < 0 и y < 0";
+  if (a == 4) res = "x > 0 и y < 0";
+  return res;
+}
+
+string result;
+if (num > 0 && num < 5)
+{
+  result = Coord(num);
+}
+else
+{
+  result = "некорректный ввод";
+}
+
+Console.WriteLine(result);
