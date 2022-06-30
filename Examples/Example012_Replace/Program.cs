970dce5a16e8b88b6a09b6b043f6d03dc7c13e85
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-06-30 14:26:19 +0300

    add Examle012_Replace

---
+++ Examples/Example012_Replace/Program.cs
@@ -0,0 +1,20 @@
+﻿// Дан текст. Заменить пробелы черточками.
+
+string text = "Что нужно сделать первым делом? князь";
+
+string Replace(string txt, char oldValue, char newValue)
+{
+  int length = txt.Length;
+  string newTxt = String.Empty;
+  for (int i = 0; i <= length - 1; i++)
+  {
+    // if (txt[i] == oldValue) newTxt = $"{newTxt}{newValue}";
+    // newTxt = $"{newTxt}{txt[i]}";
+    if (txt[i] == oldValue) newTxt = newTxt + $"{newValue}";
+    else newTxt = newTxt + $"{txt[i]}";
+  }
+  return newTxt;
+}
+
+string newText = Replace(text,'к','К');
+Console.WriteLine(newText);
