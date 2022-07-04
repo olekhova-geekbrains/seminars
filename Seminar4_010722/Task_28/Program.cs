2b39d2ca93ab964c97e48bc2eaf7642f93e7acf2
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-04 21:40:00 +0300

    add Task_28

---
+++ Seminar4_010722/Task_28/Program.cs
@@ -0,0 +1,20 @@
+﻿//Task_28 На вход число N, на выход произведение от 1 до N
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int num)
+{
+  int proud = 1;
+  for (int i = 1; i <= num; i++)
+  {
+    checked
+    {
+      proud *= i;
+    }
+  }
+  return proud;
+}
+
+int factorial = Factorial(number);
+Console.WriteLine(factorial);
