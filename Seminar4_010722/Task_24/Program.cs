819b9070502f0f984fc5d2f60298a056482c19a6
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-04 21:30:26 +0300

    add Task_24

---
+++ Seminar4_010722/Task_24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Task_24
+//На вход число А, на выход сумма чисел от 1 до А
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
+// for (int i = 1; i <= number; i++)
+// {
+//     sum += i;
+//     if (i < number) Console.Write($"{i} + ");
+//     else Console.Write($"{i} = ");
+// }
+// Console.WriteLine(sum);
+
+int Sum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int summer = Sum(number);
+Console.WriteLine($"Сумма чисел от 1 до {number}: {summer}");
