e023dbc609cf0ef3572ab10f8eb2f7768b2cf94a
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-04 21:32:32 +0300

    add Task_26

---
+++ Seminar4_010722/Task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task_26 на вход число, на выход количество цифр в этом числе
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int Count(int num)
+{
+    int localNum = num;
+    int count = 0;
+    while (localNum != 0)
+    {
+        localNum /= 10;
+        count += 1;
+    }
+    return count;
+}
+int count = Count(number);
+Console.WriteLine(count);
