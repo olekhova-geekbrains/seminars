142d218bb326f494104363a7b4e90765a4ec8668
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-05 18:48:10 +0300

    add task_33

---
+++ Seminar5_050722/Task_33/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 33: Задайте массив.Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве. 
+// массив[6, 7, 19, 345, 3]->нет
+// массив[6, 7, 19, 345, 3]->да 
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int[] array = { 6, 7, 19, 345, 3 };
+
+string CheckNumber(int[] arr, int num)
+{
+  string res = "нет"; 
+  for (int i = 0; i < arr.Length; i++) 
+  { 
+    if (arr[i] == num) res = "да"; 
+  }
+  return res;
+}
+string result = CheckNumber(array, number); 
+Console.WriteLine(result);
