caef78b4bb32ab9eadc1ed75a3ed19501e94bec1
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-06 16:39:57 +0300

    add task_34

--- Homework5/Task_34/Program.cs
+++ Homework5/Task_34/Program.cs
@@ -2,7 +2,6 @@
 // случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество 
 // чётных чисел в массиве.
-
 // [345, 897, 568, 234] -> 2
 
 int[] CreateArrayRndInt(int size, int minElement, int maxElement)
@@ -34,7 +33,7 @@ int EvenNumbers(int[] arr)
   int count = 0;
   for (int i = 0; i < arr.Length; i++)
   {
-    if(arr[i] % 2 == 0) count++;
+    if (arr[i] % 2 == 0) count++;
   }
   return count;
 }
