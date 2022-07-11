cb8cb20f5fd9958c8f8ffe417ca6a0ecc1bd16bd
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-11 15:56:34 +0300

    add Task42

---
+++ Seminar6_080722/Task_42/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string res = "";
+while(number > 0)
+{
+  res = number % 2 + res;
+  number /= 2;
+}
+
+// string Reverse(string res)
+// {
+//   string newRes = "";
+//   for (int i = res.Length - 1; i >= 0; i--)
+//   {
+//     newRes += res[i];
+//   }
+//   return newRes;
+// }
+
+// string result = Reverse(res);
+// Console.WriteLine(result);
+Console.WriteLine(res);
