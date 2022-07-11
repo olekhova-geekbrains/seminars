2bf8702603ef698c1166466e0973bafb11e6e450
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-11 16:29:57 +0300

    add Task45

--- Seminar6_080722/Task_44/Program.cs
+++ Seminar6_080722/Task_44/Program.cs
@@ -3,3 +3,30 @@
 // Если N = 5-> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.WriteLine("Ведите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if (i == 0) Console.Write("[");
+    if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+    else Console.WriteLine(arr[i] + "]");
+  }
+}
+
+int[] CreateFibonacci(int num)
+{
+  int[] fibon = new int[num]{0, 1};
+
+  for (int i = 2; i < fibon.Length; i++)
+  {
+    fibon[i] = fibon[i-1] + fibon[i-2];
+  }
+  return fibon;
+}
+
+int[] fibonacci = CreateFibonacci(number);
+PrintArray(fibonacci);
