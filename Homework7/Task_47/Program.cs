c039a5ac7a79b585b9fda8efeedf9149b37aa0d1
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-12 14:09:10 +0300

    add Task47

---
+++ Homework7/Task_47/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 47.Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальный элемент: ");
+int minElement = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальный элемент: ");
+int maxElement = Convert.ToInt32(Console.ReadLine());
+
+double[,] CreateMatrixRndDouble(int m, int n, double min, double max)
+{
+  double[,] array = new double[m, n];
+  var rnd = new Random();
+
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = Math.Round(rnd.NextDouble()*(max - min) - min, 2);
+    }
+  }
+  return array;
+}
+
+void PrintMatrix(double[,] arr)
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      if (j == 0) Console.Write("[");
+      if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ", ");
+      else Console.Write(arr[i, j] + "]");
+    }
+    Console.WriteLine();
+  }
+}
+
+double[,] matrix = CreateMatrixRndDouble(num1, num2, minElement, maxElement);
+PrintMatrix(matrix);
