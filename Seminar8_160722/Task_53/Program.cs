64b052c30f202d766d07df157346823ade451098
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-17 19:42:21 +0300

    add task53

---
+++ Seminar8_160722/Task_53/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+// которая поменяет местами первую и последнюю строку
+// массива.
+
+Console.WriteLine("Введите количество строк: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальный элемент: ");
+int minElement = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальный элемент: ");
+int maxElement = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ", ");
+            else Console.Write(arr[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+void ChangeLine(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        int temp = arr[0, i];
+        arr[0, i] = arr[arr.GetLength(0) - 1, i];
+        arr[arr.GetLength(0) - 1, i] = temp;
+
+    }
+}
+
+int[,] matrix = CreateMatrixRndInt(num1, num2, minElement, maxElement);
+PrintMatrix(matrix);
+Console.WriteLine();
+ChangeLine(matrix);
+PrintMatrix(matrix);
