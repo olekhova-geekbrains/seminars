d77205943daae1cb3da070ec98d6921e1bbaf312
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-21 22:21:53 +0300

    add task56

---
+++ Homework8/Task_56/Program.cs
@@ -0,0 +1,79 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку 
+// с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.WriteLine("Введите количество строк: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальный элемент: ");
+int minElement = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимальный элемент: ");
+int maxElement = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    var rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ", ");
+            else Console.Write(arr[i, j] + "]");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumRow(int[,] arr, int row)
+{
+    int sum = arr[row, 0];
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        sum += arr[row, i];
+    }
+    return sum;
+}
+
+void minSumRow(int[,] arr)
+{
+    int minSum = SumRow(arr, 0);
+    int minIndex = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = SumRow(arr, i);
+        if (sum < minSum)
+        {
+            minSum = sum;
+            minIndex = i;
+        }
+    }
+    Console.WriteLine($"{minIndex} строка");
+}
+
+int[,] matrix = CreateMatrixRndInt(num1, num2, minElement, maxElement);
+PrintMatrix(matrix);
+Console.WriteLine();
+minSumRow(matrix);
