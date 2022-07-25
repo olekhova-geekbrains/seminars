2b6266b9e4a65b92319b5fb649363d72fc2201c9
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-25 16:50:58 +0300

    add Task70

---
+++ Seminar10_220722/Task_70/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 70: Напишите программу, которая на вход принимает два числа и выдаёт
+// первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// 3 и 4, N = 5-> 3 4 7 11 18
+// 6 и 10, N = 4-> 6 10 16 26
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число - количество чисел в последовательности: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// string result = GetFibonacci(num1, num2, num);
+// Console.WriteLine(result);
+
+for (int i = 0; i < num; i++)
+{
+  Console.Write($"{GetFibonacci(num1, num2, i)} ");
+}
+
+int GetFibonacci(int a, int b, int n)
+{
+  if (n == 0) return a;
+  if (n == 1) return b;
+  else return GetFibonacci(a, b, n - 1) + GetFibonacci(a, b, n - 2);
+}
