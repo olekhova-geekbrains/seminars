df9a1b613eeb93f8c90e0dec4b8dfdf9cefe15a9
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-25 14:35:27 +0300

    add Task67

---
+++ Seminar9_190722/Task_67/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = SumOfDigit(num);
+Console.WriteLine(sum);
+
+int SumOfDigit(int digit)
+{
+  if (digit > 0)
+  {
+    return digit % 10 + SumOfDigit(digit / 10);
+  }
+  return 0;
+}
