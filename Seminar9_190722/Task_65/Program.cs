58869be29fd979e1d3f332d977e655e5fb39af37
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-07-25 14:17:00 +0300

    add Task65

---
+++ Seminar9_190722/Task_65/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5-> "1, 2, 3, 4, 5"
+// M = 4; N = 8-> "4, 6, 7, 8"
+
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int numM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+
+// if(numM < numN) NaturalNumbers(numM, numN);
+// else NaturalNumbers(numN, numM);
+
+NaturalNumbers(numM, numN);
+
+void NaturalNumbers(int m, int n)
+{
+  if (m < n)
+  {
+    NaturalNumbers(m, n - 1);
+    Console.Write($"{n} ");
+  }
+  if (m > n)
+  {
+    NaturalNumbers(m, n + 1);
+    Console.Write($"{n} ");
+  }
+  if (m == n)
+  {
+    Console.Write($"{n} ");
+  }
+}
